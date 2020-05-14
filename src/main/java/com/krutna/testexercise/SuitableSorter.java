f29cfd134146821d47914dbc7e6fd0dfcbbc3e48
KrutNA <krutna@pm.me>
2020-05-14 11:57:02 +0300

    Added javadoc for

--- src/main/java/com/krutna/testexercise/SuitableSorter.java
+++ src/main/java/com/krutna/testexercise/SuitableSorter.java
@@ -4,9 +4,18 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.IntPredicate;
 import java.util.stream.IntStream;
 
-class SuitableSorter {
+/** Class which represents some functionality for sorting only suitable objects. */
+public class SuitableSorter {
 
-  public final int[] sortOnlyFiltered(final int[] array, IntPredicate function) {
+  /**
+   * Selecting from <code>array</code> and sorting only parts which suitable to <code>function
+   * </code>
+   *
+   * @param array <code>int[]</code> on base of which will be created partly sorted new.
+   * @param function <code>IntPredicate</code> which will be used for selectig sortable elements.
+   * @return New <code>int[]</code> which result of merge <code>array</code> with sorted elements.
+   */
+  public final int[] sortOnlyFiltered(final int[] array, final IntPredicate function) {
 
     final AtomicInteger index = new AtomicInteger();
     final int[] filtered = IntStream.of(array).parallel().filter(function).sorted().toArray();
