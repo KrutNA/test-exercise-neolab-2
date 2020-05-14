d930f9342b440fb7c7614225f07a7c8a45f82e98
KrutNA <krutna@pm.me>
2020-05-14 12:44:15 +0300

    Added javadoc for App

--- src/main/java/com/krutna/testexercise/App.java
+++ src/main/java/com/krutna/testexercise/App.java
@@ -1,25 +1,48 @@
 package com.krutna.testexercise;
 
+/** Class which represents of <code>main</code> method and base functionallitty of application */
 public class App {
 
   final SuitableSorter sorter;
   final TimeProcessor timeProcessor;
-  final Chess chessboard;
+  final Chessboard chessboard;
 
+  /**
+   * Base contructor with constructing <code>sorter</code>, <code>timeProcessor</code> and <code>
+   * chessboard</code>
+   */
   App() {
     sorter = new SuitableSorter();
     timeProcessor = new TimeProcessor();
-    chessboard = new Chess(4, 4);
+    chessboard = new Chessboard(4, 4);
   }
 
+  /**
+   * Sorts only odds of <code>array</code>
+   *
+   * @param array Integer array whose odds will be sorted
+   * @return New <code>int[]</code> with sorted odds
+   * @see SuitableSorter#sortOnlyFiltered(int[], IntPredicate)
+   */
   public int[] sortOdds(final int[] array) {
     return this.sorter.sortOnlyFiltered(array, x -> x % 2 == 1);
   }
 
+  /**
+   * This function correct time value like <code>8:70:70</code> to <code>09:11:10</code>
+   *
+   * @see TimeProcessor#correct(String)
+   */
   public String correctTime(final String time) throws IllegalArgumentException {
     return this.timeProcessor.correct(time);
   }
 
+  /**
+   * Checks checkmate for black <code>king</code>. If is checkmate: returns <code>true</code>,
+   * otherwise <code>false</code>
+   *
+   * @see Chessboard#check(String)
+   */
   public boolean checkChess(final String chess) throws IllegalArgumentException {
     return this.chessboard.check(chess);
   }
